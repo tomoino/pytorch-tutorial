bd4962856b134132b0217bc8322844079b2a2c0f
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2021-10-17 21:35:45 +0900

    [add] docker

---
+++ docker/env.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# Set environment variables.
+
+export IMAGE_NAME=${USER}_pytorch_tutorial
+export CONTAINER_NAME=${USER}_pytorch_tutorial
+export JUPYTER_HOST_PORT=18885
+export JUPYTER_CONTAINER_PORT=18885
