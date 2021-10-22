16d292c2083794b0a5dffd1abfee0c7e4f1d1017
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2021-10-22 09:55:52 +0900

    [delete] unnecessary docker files

--- docker/env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Set environment variables.
-
-export IMAGE_NAME=${USER}_pytorch_tutorial
-export CONTAINER_NAME=${USER}_pytorch_tutorial
-export JUPYTER_HOST_PORT=18885
-export JUPYTER_CONTAINER_PORT=18885
